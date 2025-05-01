3a9b6e85340db6381721302cd8a8a4331a54f729
DrupiFan <sabakublashvili505@gmail.com>
2025-05-02 00:36:50 +0400

    followersfunctiom

--- firstlecture/src/Card.jsx
+++ firstlecture/src/Card.jsx
@@ -3,9 +3,11 @@ import { useState } from 'react'
 export default function Card(props){
 
     const [isFollowing, setIsFollowing] = useState(false);
+    const [followerCount, setFollowerCount] = useState(0);
 
     function follow(){
         setIsFollowing(!isFollowing);
+        isFollowing ? setFollowerCount(followerCount - 1) : setFollowerCount(followerCount + 1);
     }
     return (
         <div className="card">
@@ -16,6 +18,7 @@ export default function Card(props){
             />
             <h2 className="username">{props.username}</h2>
             <button className="follow-button" onClick={follow}>{isFollowing ? "Following!" : "Follow"}</button>
+            <p>{followerCount} Followers</p>
         </div>
     );
 }
