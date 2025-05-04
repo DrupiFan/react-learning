4712456423602285853db92fc13e9d7ec5e39b37
DrupiFan <sabakublashvili505@gmail.com>
2025-05-04 16:06:27 +0400

    countries-api-v2

--- api-project-react/src/App.jsx
+++ api-project-react/src/App.jsx
@@ -9,14 +9,7 @@ function App() {
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
     .then((res) => res.json())
-    .then((data) => {
-      const formatted = data.map((country) => ({
-        name: country.name.common,
-        flag: country.flags.svg,
-        capital: country.capital,
-      }));
-      setCountries(formatted)
-    });
+    .then((data) => setCountries(data))
   }, []);
 
   return (
@@ -24,8 +17,8 @@ function App() {
       <div className="app">
       <h1>Country Flags</h1>
       <div className="grid">
-        {countries.map((country, index) => (
-          <Card key={index} name={country.name} flag={country.flag} capital={country.capital} />
+        {countries.map((country, i) => (
+          <Card key={i} name={country.name.common} flag={country.flags.svg} capital={country.capital} />
         ))}
       </div>
     </div>
